d78ccb44374c227a99485e5fb1d16762c9f96105
Eton Hsu <etonhsu111@gmail.com>
2024-05-29 08:53:07 -0700

    adjusted configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['recharts'],
+  }},
   optimizeDeps: {
     exclude: ['chunk-HKLPI2XQ', 'chunk-YI7ZP7WZ']
   }
-});
+})
+
